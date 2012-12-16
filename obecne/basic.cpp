450bfde8f5926462f9f0ba654497ab3deb4892a0
ondrap <ondrap@ondrap.cz>
2012-12-16 21:13:22 +0100

    Vytvoreni slozky obecne s basic.cpp a basic.h. basic.h obsahuje zakladni funkce a struktury pro robotiku. basic.cpp implementuje funkce v basic.h
    Zatim jen vytvoreny hlavicky funkci pro normalizovani uhlu
    a generovani nahodnych cisel s normalnim rozdelenim.

---
+++ obecne/basic.cpp
@@ -0,0 +1,12 @@
+#include "basic.h"
+
+basic_robotic_fce::normAngle(double Angle){
+
+}
+
+basic_robotic_fce::randn_notrig(double Mu, double Sigma){
+
+}
+
+
+
---
+++ obecne/basic.h
@@ -0,0 +1,52 @@
+/*!
+	\brief 	Basic structur, class and function for robotic usage
+	*	Units for position is in meters. Angle use radian.
+
+	\author Ondrej Pilat
+	\date 3/12/2012
+	\version 0.1
+	\note web: ondrap.cz mail: ondrap@ondrap.cz
+*/
+
+#ifndef BASIC_H
+#define BASIC_H
+
+/*!
+	\struct position
+	\brief Position structure for robot or object in 2D world.
+*/
+struct position{
+	double x;
+	double y;
+
+	position(double X = 0, double Y = 0){x = X; y = Y;};
+};
+
+/*!
+	\struct state
+	\brief State for robot. Inherit x,y from position structure.
+*/
+struct state : public position {
+	double angle;
+
+	state(double X = 0, double Y = 0, double Angle = 0) : position(X,Y)
+	{angle = Angle;};
+};
+
+namespace basic_robotic_fce{
+	//!	Function returns normalized input angle in range -Pi to Pi
+	/*!	
+		\param Angle an double argument.
+		\return normalized input angle
+	*/
+	double normAngle(double Angle);
+	//!	Function for random number witch use normal distribution.
+	/*!
+		\param Mu an double argument. Mu is center of normal distribution.
+		\param Sigma an double argument. Sigma describe dispersion.
+		\return Random number with normal distribution with center Mu and Sigma.
+	*/
+	double randn_notrig(double Mu=0.0, double Sigma=1.0);
+}
+
+#endif
