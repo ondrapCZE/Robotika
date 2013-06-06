ef5bc00ca0185e194e3e542efee3e8226ca53c0c
ondrap <ondrap@ondrap.cz>
2013-06-06 10:32:32 +0200

    Vytvoren bash script pro spusteni Bear rescue.

---
+++ BearRescue/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+gpio mode 0 up
+STATE=$(gpio read 0)
+echo $STATE
+while [ $STATE -ne 0 ]
+do
+	clear
+	echo Press button for start
+	STATE=$(gpio read 0)
+	sleep 0.1
+done
+echo Start bear rescue
