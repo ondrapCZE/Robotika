a610a71be9479c879a0fb4c1581ee37f08e86b31
ondrap <ondrap@ondrap.cz>
2013-06-07 21:51:27 +0200

    Posledni predzavodni upravy.

--- BearRescue/run.sh
+++ BearRescue/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+sudo chmod 0666 /dev/bus/usb/001/004
 gpio mode 0 up
 STATE=$(gpio read 0)
 echo $STATE
@@ -10,4 +11,4 @@ do
 	sleep 0.1
 done
 echo Start bear rescue
-./Plan/BearRescue
+./Plan/BearRescue > logRun
