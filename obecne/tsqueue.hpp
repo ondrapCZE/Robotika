021c3ae4b0305cf84588dc5a5a64b5045ed8d3fa
ondrap <ondrap@ondrap.cz>
2013-12-18 10:53:27 +0000

    Complete implementation of tsqueue.

--- obecne/tsqueue.hpp
+++ obecne/tsqueue.hpp
@@ -1,33 +1,49 @@
 #ifndef TSQUEUE_H
 #define TSQUEUE_H
 
-#include <queue>
+#include <deque>
 #include <mutex>
 #include <condition_variable>
 
 template <class T>
 class tsqueue{
-	std::queue<T> dataQueue;
+	std::deque<T> dataDeque;
 	
 	std::mutex lck;
 	std::condition_variable emptyCondition;
 public:
 	void push(T element){
 		std::lock_guard<std::mutex> guard(lck);
-		dataQueue.push(element);
+		dataDeque.push_back(element);
 		emptyCondition.notify_one();
 	};
 	
+	bool tryPop(T &element){
+		std::lock_guard<std::mutex> guard(lck);		
+		if(dataDeque.empty())
+			return false;
+		
+		element = dataDeque.front();
+		dataDeque.pop_front();
+		return true;
+	}
+	
 	T pop(){
-		return dataQueue.front();
+		std::unique_lock<std::mutex> uq_lck(lck);
+		emptyCondition.wait(uq_lck,[bool](){return !dataDeque.empty();});
+		T copy = dataDeque.front();
+		dataDeque.pop_front();
+		return copy;
 	};
 	
 	bool empty(){
-		return true;
+		std::lock_guard<std::mutex> guard(lck);
+		return dataDeque.empty();
 	};
 	
 	void clear(){
-	
+		std::lock_guard<std::mutex> guard(lck);
+		dataDeque.clear();
 	};
 };
 
