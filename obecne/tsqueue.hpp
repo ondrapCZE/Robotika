223eb56a986a03b9c051769febf2d2ef8e3d69f7
ondrap <ondrap@ondrap.cz>
2014-09-24 11:31:29 +0200

    Tsqueue: add methods for manipulating with end of the queue

--- obecne/tsqueue.hpp
+++ obecne/tsqueue.hpp
@@ -5,60 +5,92 @@
 #include <mutex>
 #include <condition_variable>
 
-template <class T>
-class tsqueue{
+template<class T>
+class tsqueue {
 	std::deque<T> dataDeque;
-	
+
 	std::mutex lck;
 	std::condition_variable emptyCondition;
 public:
-	void push_back(T element){
-		std::lock_guard<std::mutex> guard(lck);
+	void push_back(T element) {
+		std::lock_guard < std::mutex > guard(lck);
 		dataDeque.push_back(element);
 		emptyCondition.notify_one();
-	};
-	
-	void push_front(T element){
-		std::lock_guard<std::mutex> guard(lck);
+	}
+	;
+
+	void push_front(T element) {
+		std::lock_guard < std::mutex > guard(lck);
 		dataDeque.push_front(element);
 		emptyCondition.notify_one();
-	};
+	}
+	;
 
-	bool pop_front(){
-		std::lock_guard<std::mutex> guard(lck);		
-		if(dataDeque.empty())
+	bool pop_front() {
+		std::lock_guard < std::mutex > guard(lck);
+		if (dataDeque.empty())
 			return false;
-		
+
 		dataDeque.pop_front();
 		return true;
-	}
-	
-	bool try_front(T &element){
-		std::lock_guard<std::mutex> guard(lck);		
-		if(dataDeque.empty())
+	};
+
+	bool pop_back() {
+		std::lock_guard < std::mutex > guard(lck);
+		if (dataDeque.empty())
 			return false;
-		
+
+		dataDeque.pop_back();
+		return true;
+	};
+
+	bool try_front(T &element) {
+		std::lock_guard < std::mutex > guard(lck);
+		if (dataDeque.empty())
+			return false;
+
 		element = dataDeque.front();
 		return true;
-	}
-	
-	T front(){
-		std::unique_lock<std::mutex> uq_lck(lck);
-		emptyCondition.wait(uq_lck,[this]{return !dataDeque.empty();});
+	};
+
+	bool try_back(T &element) {
+		std::lock_guard < std::mutex > guard(lck);
+		if (dataDeque.empty())
+			return false;
+
+		element = dataDeque.back();
+		return true;
+	};
+
+	T front() {
+		std::unique_lock < std::mutex > uq_lck(lck);
+		emptyCondition.wait(uq_lck, [this] {return !dataDeque.empty();});
 		T copy = dataDeque.front();
 		dataDeque.pop_front();
 		return copy;
-	};
-	
-	bool empty(){
-		std::lock_guard<std::mutex> guard(lck);
+	}
+	;
+
+	T back() {
+		std::unique_lock < std::mutex > uq_lck(lck);
+		emptyCondition.wait(uq_lck, [this] {return !dataDeque.empty();});
+		T copy = dataDeque.back();
+		dataDeque.pop_back();
+		return copy;
+	}
+	;
+
+	bool empty() {
+		std::lock_guard < std::mutex > guard(lck);
 		return dataDeque.empty();
-	};
-	
-	void clear(){
-		std::lock_guard<std::mutex> guard(lck);
+	}
+	;
+
+	void clear() {
+		std::lock_guard < std::mutex > guard(lck);
 		dataDeque.clear();
-	};
+	}
+	;
 };
 
 #endif
